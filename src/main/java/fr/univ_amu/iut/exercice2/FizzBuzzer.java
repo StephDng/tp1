941d09de9ae54432b33c65cd870f90d615d8342f
Stéphanie Dang <stephanie.dang@etu.univ-amu.fr>
2020-04-29 16:21:33 +0200

    Exercice2

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -5,7 +5,7 @@ public class FizzBuzzer {
     public String computeString(int i) {
         if (i == 1){
             return "1";}
-        return"0"
+        return"0";
     }
 
     public String[] computeList(int i) {
