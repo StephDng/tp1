0f71a57aa732fc83d854493b1df11ecace4ba260
Stéphanie Dang <stephanie.dang@etu.univ-amu.fr>
2020-04-29 15:57:47 +0200

    Exercice1

--- src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
+++ src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -3,9 +3,8 @@ package fr.univ_amu.iut.exercice1;
 public class HelloWorld {
     public static String hello(String s) {
 
-        if (s.isEmpty()) {
-            return "Hello, World!";
-        }
-        return "";
+        if (s == null || s.isEmpty())
+           return "Hello, World!";
+        return "Hello, " + s + "!";
     }
 }
